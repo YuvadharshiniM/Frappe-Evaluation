177de486ab65f218db9e95e54d917b37b2d57b1b
YuvadharshiniM <yuvadharshinim05@gmail.com>
2026-09-22 14:00:59 +0000

    first commit

---
+++ quickfix/quickfix/doctype/spare_part/spare_part.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2026, Yuvadharshini M and contributors
+# For license information, please see license.txt
+
+# import frappe
+import frappe
+from frappe.model.document import Document
+from frappe.model.naming import make_autoname
+
+class SparePart(Document):
+
+    def autoname(self):
+        if self.part_code:
+            self.part_code = self.part_code.upper()
+
+        self.name = make_autoname("PART-.YYYY.-.####")
+
+    def validate(self):
+        if self.selling_price <= self.unit_cost:
+            frappe.throw(
+                "Selling Price must be greater than Unit Cost."
+            )
